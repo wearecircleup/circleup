52fbb13141b1a7814e2cfdced145ac5c5a884538
daniel.munevar <daniel.munevar@rappi.com>
2024-07-22 07:52:13 -0500

    Streamlit Features

--- pages/accept_volunteer.py
+++ pages/accept_volunteer.py
@@ -63,7 +63,7 @@ def manage_volunteer_requests(connector: Firestore):
     selected_status = st.selectbox("Seleccione el estado de las solicitudes:", status_options)
 
     # Cargar solicitudes de voluntarios según el estado seleccionado
-    volunteer_requests = connector.query_collection('volunteer_request', [FieldFilter('status', '==', selected_status)])
+    volunteer_requests = connector.query_collection('volunteer_request', [('status', '==', selected_status)])
     
     # Crear lista de voluntarios para selección
     volunteer_list = [f"{req.data.get('first_name')} {req.data.get('last_name')} - {req.data.get('email')}" for req in volunteer_requests]
